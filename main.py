c06fe9e83bc00896d6199a09bc531d97ac6f53f7
thalibanallaam <thalibanallaam@gmail.com>
2025-02-04 15:21:23 +0700

    Latihan API

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+import pandas as pd
+
+#Create API Object
+app = FastAPI()
+
+#Read Data
+data = pd.read_csv('data.csv')
+
+# Coba root home API (get)
+@app.get("/")
+def root():
+    return {'message': 'Hello HCK-025'}
+
+# endpoint sapaan
+@app.get("/name/{name}")
+def greet(name):
+    return {'message': f'Hai {name}, how are you?'}
+
+#Endpoint return data
+@app.get("/data")
+def get_data(): 
+    return data.to_dict(orient='records')
+
+#Get data by ID
+@app.get("/data/{id}")
+def search_data(id:int):
+    result = data[data['id'] == id]
+    return{'result': result.to_dict(orient='records')}
+
+#Menambahkan data
+@app.post("/data/{new_data}")
+def add_data(new_data):
+    new_row = pd.DataFrame(new_data)
+    data = pd.concat([data, new_row], ignore_index= True)
+    return {'message': 'Data is updated!'}
